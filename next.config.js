c13305c50fa84cc7a0656a063d2d47ac3eefb864
ueken65 <ueken6522@gmail.com>
2023-01-02 21:00:42 +0900

    feat: ディレクトリ構成変更

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+/* eslint @typescript-eslint/no-var-requires: "off" */
+const { resolve } = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['~'] = resolve(__dirname, 'src')
+    return config
+  },
 }
 
 module.exports = nextConfig
